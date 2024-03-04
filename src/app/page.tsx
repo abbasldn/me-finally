ecfa824fc63d1234d6dedeb66c82946a9f3d3206
Abbas <abbas.hussain@outlook.com>
2024-03-04 22:04:44 +0400

    redploy to see favicon changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,7 +128,7 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Hi, I&apos;m Abbas
+            I&apos;m Abbas
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I&apos;m a software engineer based in Dubai. Co-founder and CEO of
